a4c27b594102ac6576ec82d8e74e1f021603eb82
lesegotsamaesi <lesegotsamaesi@yahoo.com>
2023-06-20 13:29:57 -0700

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
-#include "main.h"
+#include "main.h
 
-/**print_sign - Prints the sign of a number
+/** print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
