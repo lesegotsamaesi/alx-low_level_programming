4a82a749cdc802a102b029d451a19fdad1e27ddf
lesegotsamaesi <lesegotsamaesi@yahoo.com>
2023-07-06 15:02:05 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,17 +22,12 @@ int _sqrt_recursion(int n)
  *
  * Return: the natural square root
  */
-int _sqrt(int n, int i)
+int number(int n, int root)
 {
-	int sqrt = i * i;
-
-	if (sqrt > n)
+	if (n * n == root)
+		return (n);
+	if (n == root)
 		return (-1);
-
-	if (sqrt == n)
-		return (i);
-
-	return (_sqrt(n, i + 1));
-
+	return (1 * number(n + 1, root));
 }
 
